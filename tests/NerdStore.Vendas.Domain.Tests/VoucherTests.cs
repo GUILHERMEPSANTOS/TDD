008b27465096961aac782fef5404f1c55f1f62fa
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-09-22 20:59:56 -0300

    test:  add voucher tests

---
+++ tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
@@ -0,0 +1,23 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class VoucherTests
+    {
+        [Fact(DisplayName = "Validar se Voucher é valido")]
+        [Trait("Categoria", "Voucher - Pedido")]
+        public void ValidarVoucher_DataDeValidadeUtrapassada_DeveRetornarUmaException()
+        {
+            // Arrange
+            var voucher = new Voucher("ABC", DateTime.UtcNow.AddDays(-1), true, 1, Desconto.ValorFixo);
+
+            //Act & Assert
+            Assert.Throws<DomainException>(() => voucher.ValidarSeEhValido());
+        }
+    }
+}
