3260227c5e82b243d26cd8f3a0c152a7d32d7885
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-09-27 11:52:54 -0300

    feat: add commands

---
+++ src/NerdStore.Core/Messages/Command.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+
+namespace NerdStore.Core.Messages
+{
+    public abstract class Command
+    {
+        public ValidationResult ValidationResult { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        protected Command()
+        {
+               Timestamp = DateTime.UtcNow;
+        }
+
+        public abstract bool ValidarSeEhValido();
+    }
+}
