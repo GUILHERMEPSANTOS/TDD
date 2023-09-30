13e7d216fd936f82c73cd19fe17f30ca6ec34c1c
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-09-29 21:22:32 -0300

    feat: add new methods to repository

--- src/NerdStore.Vendas.Domain/Interfaces/IPedidoRepository.cs
+++ src/NerdStore.Vendas.Domain/Interfaces/IPedidoRepository.cs
@@ -5,5 +5,9 @@ namespace NerdStore.Vendas.Domain.Interfaces
     public interface IPedidoRepository : IRepository<Pedido>
     {
         void Adicionar(Pedido pedido);
+        void AdicionarItem(PedidoItem item);
+        void Atualizar(Pedido pedido);
+        Task<Pedido> ConsultarPedidoPorClientId(Guid clienteId);
+        void AtualizarItem(PedidoItem pedidoItem);
     }
 }
