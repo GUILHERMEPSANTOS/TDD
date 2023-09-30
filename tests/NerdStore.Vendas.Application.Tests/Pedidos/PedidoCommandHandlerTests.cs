4eed56de7f490508a39c7c83abbf4adf0eb1c7a9
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-09-29 21:24:12 -0300

    test: garantido que carrinho se atualiza e adiciona itens

--- tests/NerdStore.Vendas.Application.Tests/Pedidos/PedidoCommandHandlerTests.cs
+++ tests/NerdStore.Vendas.Application.Tests/Pedidos/PedidoCommandHandlerTests.cs
@@ -43,5 +43,98 @@ namespace NerdStore.Vendas.Application.Tests.Pedidos
             // mocker.GetMock<IMediator>().Verify(mediator => mediator.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once);
             ;
         }
+
+        [Fact(DisplayName = "Adicionar novo item no pedido existente")]
+        [Trait("Categoria", "PedidoCommandHandlerTests")]
+        public async Task PedidoCommandHandler_NovoItemAoPedidoExistente_DeveAdicionarComSucesso()
+        {
+            //Assert
+            var clienteId = Guid.NewGuid();
+            var pedido = Pedido.PedidoFactory.GerarNovoPedido(clienteId);
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Livro", 2, 100);
+
+            pedido.AdicionarItem(pedidoItem);
+            
+            var itemCommand = new AdicionarItemCommand(
+                   produtoId: Guid.NewGuid(),
+                   clienteId: clienteId,
+                   nome: "Livro",
+                   quantidade: 2,
+                   valorUnitario: 100
+            );
+
+            var mocker = new AutoMocker();
+            var pedidoCommandHandler = mocker.CreateInstance<PedidoCommandHandler>();
+            mocker.GetMock<IPedidoRepository>().Setup(repository => repository.ConsultarPedidoPorClientId(clienteId)).Returns(Task.FromResult(pedido));
+            mocker.GetMock<IPedidoRepository>().Setup(repository => repository.UnitOfWork.Commit()).Returns(Task.FromResult(true));
+
+            //Act
+            var result = await pedidoCommandHandler.Handle(itemCommand, CancellationToken.None);
+
+            //Assert
+            Assert.True(result);
+            mocker.GetMock<IPedidoRepository>().Verify(repository => repository.AdicionarItem(It.IsAny<PedidoItem>()), Times.Once);
+            mocker.GetMock<IPedidoRepository>().Verify(repository => repository.Atualizar(It.IsAny<Pedido>()), Times.Once);
+            mocker.GetMock<IPedidoRepository>().Verify(repository => repository.UnitOfWork.Commit(), Times.Once);                      
+        }
+
+        [Fact(DisplayName = "Atualizar item no pedido existente")]
+        [Trait("Categoria", "PedidoCommandHandlerTests")]
+        public async Task PedidoCommandHandler_ItemExistente_DeveAtualizarComSucesso()
+        {
+            //Assert
+            var clienteId = Guid.NewGuid();
+            var produtoId = Guid.NewGuid();
+
+            var pedido = Pedido.PedidoFactory.GerarNovoPedido(clienteId);
+            var pedidoItem = new PedidoItem(produtoId, "Livro", 2, 100);
+
+            pedido.AdicionarItem(pedidoItem);
+
+            var itemCommand = new AdicionarItemCommand(
+                   produtoId: produtoId,
+                   clienteId: clienteId,
+                   nome: "Livro",
+                   quantidade: 2,
+                   valorUnitario: 100
+            );
+
+            var mocker = new AutoMocker();
+            var pedidoCommandHandler = mocker.CreateInstance<PedidoCommandHandler>();
+            mocker.GetMock<IPedidoRepository>().Setup(repository => repository.ConsultarPedidoPorClientId(clienteId)).Returns(Task.FromResult(pedido));
+            mocker.GetMock<IPedidoRepository>().Setup(repository => repository.UnitOfWork.Commit()).Returns(Task.FromResult(true));
+
+            //Act
+            var result = await pedidoCommandHandler.Handle(itemCommand, CancellationToken.None);
+
+            //Assert
+            Assert.True(result);
+            mocker.GetMock<IPedidoRepository>().Verify(repository => repository.AtualizarItem(It.IsAny<PedidoItem>()), Times.Once);
+            mocker.GetMock<IPedidoRepository>().Verify(repository => repository.Atualizar(It.IsAny<Pedido>()), Times.Once);
+            mocker.GetMock<IPedidoRepository>().Verify(repository => repository.UnitOfWork.Commit(), Times.Once);
+        }
+
+        [Fact(DisplayName = "Command inválido")]
+        [Trait("Categoria", "PedidoCommandHandlerTests")]
+        public async Task PedidoCommandHandler_CommandInvalido_DeveRetornarFalsoELancarEventosDeDominio()
+        {
+            // Assert
+            var itemCommand = new AdicionarItemCommand(
+                   produtoId: Guid.Empty,
+                   clienteId: Guid.Empty,
+                   nome: "",
+                   quantidade: 0,
+                   valorUnitario: 0
+            );
+            var mocker = new AutoMocker();
+            var pedidoCommandHandler = mocker.CreateInstance<PedidoCommandHandler>();
+
+            //Act
+            var result = await pedidoCommandHandler.Handle(itemCommand, CancellationToken.None);
+
+            //Assert
+            Assert.False(result);
+            mocker.GetMock<IMediator>().Verify(mediator => mediator.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Exactly(5));
+        }
     }
 }
